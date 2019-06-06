ed8da1f09cc2d8951f92a46f8ec1fa0a88e0368f
Jack Park <jackpark@gmail.com>
2019-06-06 16:29:20 -0700

    typo

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xms1024M -Xmx4G -cp ./target/tqks-statistics-server-0.1.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.ks.statistics.Main
+java -Xms1024M -Xmx4G -cp ./target/tqos-statistics-server-0.1.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.ks.statistics.Main
