8a3e09bc8cd42335f0e0f81f2af005e08c9dbc5a
Jack Park <jackpark@gmail.com>
2019-06-10 15:59:11 -0700

    fixed jar version

--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-java -cp ./target/tqks-statistics-server-0.2.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.ks.statistics.Stopper
+java -cp ./target/tqks-statistics-server-0.3.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.ks.statistics.Stopper
